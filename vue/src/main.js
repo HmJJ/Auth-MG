a422055cf80834031dd7cd4d27000fe281a93234
Nott <1564223954@qq.com>
2018-10-26 14:40:21 +0800

    update

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,27 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import routes from './routes'
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-const app = new Vue({
+new Vue({
   el: '#app',
-  data: {
-    currentRoute: window.location.pathname
-  },
-  computed: {
-    ViewComponent () {
-      const matchingView = routes[this.currentRoute]
-      return matchingView
-        ? require('./view/test/' + matchingView + '.vue')
-        : require('./view/error/404')
-    }
-  },
-  render (h) {
-    return h(this.ViewComponent)
-  }
-})
-
-window.addEventListener('popstate', () => {
-  app.currentRoute = window.location.pathname
+  router,
+  components: { App },
+  template: '<App/>'
 })
